fd007d3be25002e3287211357d7bf39901eeee28
Vladislav Belov <vladik.by@gmail.com>
2017-09-18 21:56:41 +0300

    правка алгоритма

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -10,71 +10,88 @@ class GraphsController < ApplicationController
   def charts
     @charts ||=
         begin
-          time = [24.hours.ago, Time.parse('2017-09-15 17:00:00 +0300')].max
-          glasses = Glass.where(
-              stock_code: stock_names,
-              target_code: target_currency,
-              base_code: base_currency,
-          ).where('created_at > ?', time).order(:created_at).to_a
+          active_stocks = []
+          pair_of_actions = %i(sell buy)
+          points_by_stock = {}
+
+          stock_names.each do |stock_code|
+            time = [24.hours.ago, Time.parse('2017-09-15 17:00:00 +0300')].max
+            glasses = Glass.where(
+                stock_code: stock_code,
+                target_code: target_currency,
+                base_code: base_currency,
+            ).where('created_at > ?', time).order(:created_at).to_a
+
+            if glasses.any?
+              active_stocks << stock_code
+              stock = Stocks.const_get(stock_code).new
+
+              stock_points = benchmark "processing glasses for stock #{stock_code}" do
+                pair_of_actions.map do |action|
+                  points = glasses.map do |glass|
+                    rate = stock.process_glass_fast(glass, action, volume)
+                    [glass.created_at, rate]
+                  end
+                  [action, points]
+                end.to_h
+              end
+
+              points_by_stock[stock_code] = stock_points
+            end
+          end
 
           arr = []
 
-          stock_names.permutation(2).each do |pair_of_stocks|
-            pair_of_actions = %i(sell buy)
-            buy_points, sell_points = benchmark("processing glasses for a pair of stocks") do
-              2.times.map do |idx|
-                stock_code = pair_of_stocks[idx]
-                action = pair_of_actions[idx]
-                stock = Stocks.const_get(stock_code).new
-                gg = glasses.select { |g| g.stock_code == stock_code }
-                gg.map do |glass|
-                  rate = stock.process_glass_fast(glass, action, volume)
-                  [glass.created_at, rate]
-                end
-              end
-            end
-            first_buy_at = buy_points.first[0]
-            first_sell_at = sell_points.first[0]
-            first_at = [first_buy_at, first_sell_at].max
-            buy_points.select! { |p| p[0] >= first_at }
-            sell_points.select! { |p| p[0] >= first_at }
+          active_stocks.combination(2).each do |pos|
+            [pos, pos.reverse].each do |pair_of_stocks|
+              stock1 = pair_of_stocks[0]
+              stock2 = pair_of_stocks[1]
+              buy_points = points_by_stock[stock1][:buy]
+              sell_points = points_by_stock[stock2][:sell]
 
-            next unless buy_points.any? && sell_points.any?
+              first_buy_at = buy_points.first[0]
+              first_sell_at = sell_points.first[0]
+              first_at = [first_buy_at, first_sell_at].max
+              buy_points = buy_points.select { |p| p[0] >= first_at }
+              sell_points = sell_points.select { |p| p[0] >= first_at }
 
-            sell_iterator = sell_points.to_enum
-            prev_sell = sell_iterator.next
-            prev_sell_at = prev_sell[0]
-            next_sell = sell_iterator.peek
-            next_sell_at = next_sell[0]
+              next unless buy_points.any? && sell_points.any?
 
-            points = benchmark 'calculating arbitrage' do
-              buy_points.map do |point|
-                created_at = point[0]
-                while next_sell_at < created_at
-                  begin
-                    prev_sell, next_sell = next_sell, sell_iterator.next
-                    prev_sell_at, next_sell_at = next_sell_at, next_sell[0]
-                  rescue StopIteration
-                    prev_sell = next_sell
-                    prev_sell_at = next_sell_at
-                    break
+              sell_iterator = sell_points.to_enum
+              prev_sell = sell_iterator.next
+              prev_sell_at = prev_sell[0]
+              next_sell = sell_iterator.peek
+              next_sell_at = next_sell[0]
+
+              points = benchmark 'calculating arbitrage' do
+                buy_points.map do |point|
+                  created_at = point[0]
+                  while next_sell_at < created_at
+                    begin
+                      prev_sell, next_sell = next_sell, sell_iterator.next
+                      prev_sell_at, next_sell_at = next_sell_at, next_sell[0]
+                    rescue StopIteration
+                      prev_sell = next_sell
+                      prev_sell_at = next_sell_at
+                      break
+                    end
                   end
+                  if next_sell_at >= created_at
+                    take_next = (created_at - prev_sell_at) > (next_sell_at - created_at)
+                    # dt = [(created_at - prev_sell_at).abs, (next_sell_at - created_at).abs].min
+                    # puts "dt: #{dt}"
+                    sell = take_next ? next_sell : prev_sell
+                  else
+                    sell = prev_sell
+                  end
+                  buy_price = point[1]
+                  sell_price = sell[1]
+                  [created_at, (sell_price / buy_price - 1) * 100.0]
                 end
-                if next_sell_at >= created_at
-                  take_next = (created_at - prev_sell_at) > (next_sell_at - created_at)
-                  # dt = [(created_at - prev_sell_at).abs, (next_sell_at - created_at).abs].min
-                  # puts "dt: #{dt}"
-                  sell = take_next ? next_sell : prev_sell
-                else
-                  sell = prev_sell
-                end
-                buy_price = point[1]
-                sell_price = sell[1]
-                [created_at, (buy_price / sell_price - 1) * 100.0]
               end
+              title = "Покупка на #{pair_of_stocks[1]}, продажа на #{pair_of_stocks[0]}"
+              arr << {title: title, chart_data: points}
             end
-            title = "Покупка на #{pair_of_stocks[1]}, продажа на #{pair_of_stocks[0]}"
-            arr << {title: title, chart_data: points}
           end
 
           arr
